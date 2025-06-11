1d9ffbb2c471f655be63f3ad02b0007567510f7e
waseemalmazrua <gakalmoot12@gmail.com>
2025-06-11 13:33:03 +0300

    Update concat_groupby_utils.py

--- concat_groupby_utils.py
+++ concat_groupby_utils.py
@@ -19,9 +19,11 @@ def concat_dataframes(dfs_list, ignore_index=True):
     - DataFrame مدموج
     """
     return pd.concat(dfs_list, ignore_index=ignore_index)
+    
+------------------------------------------------------------------------------------
 
 
-def group_data(df, by, agg_column, agg_func='mean'):
+def group_data(df, by, agg_column, agg_func='mean', sort=False, ascending=False):
     """
     دالة لتجميع البيانات باستخدام groupby.
 
@@ -30,8 +32,23 @@ def group_data(df, by, agg_column, agg_func='mean'):
     - by: العمود أو الأعمدة التي يتم التجميع حسبها (string أو list)
     - agg_column: العمود الذي سيتم تنفيذ الدالة عليه
     - agg_func: الدالة الإحصائية ('mean', 'sum', 'count', ...)
+    - sort: هل ترغب بترتيب النتائج؟ (افتراضي = False)
+    - ascending: ترتيب تصاعدي أم تنازلي (افتراضي = False)
 
     Returns:
-    - DataFrame بعد التجميع
+    - DataFrame بعد التجميع (مع reset_index + الترتيب إذا طُلب)
     """
-    return df.groupby(by)[agg_column].agg(agg_func).reset_index()
+    grouped = df.groupby(by)[agg_column].agg(agg_func).reset_index()
+
+    if sort:
+        grouped = grouped.sort_values(by=agg_column, ascending=ascending)
+
+    return grouped
+
+#exampel for sort :
+group_data(df, by='Country', agg_column='Stars', agg_func='mean', sort=True, ascending=False)
+
+#example without sort:
+group_data(df, by='Style', agg_column='Stars', agg_func='count')
+
+
